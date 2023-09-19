056efd5068213a697457f623d3bed29f6c4cba8a
Yue Li <michaelli0914@outlook.com>
2023-09-19 04:27:01 +0000

    change main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     run_histogram(nba)
 
     str1 = f'{summary.to_markdown()}'
-    str2 = f'![Alt text](figures/points_hist.png)'
-    str3 = f'![Alt text](figures/scatter.png)'
+    str2 = '![Alt text](figures/points_hist.png)'
+    str3 = '![Alt text](figures/scatter.png)'
 
     file_path = "./report.md"
 
