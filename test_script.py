8d5802d0b6b4997cf4cc7d1c32a9979d53b0550c
Yue Li <michaelli0914@outlook.com>
2023-09-19 03:38:57 +0000

    change test_script

--- test_script.py
+++ test_script.py
@@ -1,9 +1,9 @@
-from lib import readfile, lib_describe, build_pairplot, build_scatterplot
+from lib import readfile, lib_describe, build_histogram, build_scatterplot
 
 def test_main():
     nba = readfile("nba-teams-2017.csv")
     lib_describe(nba)
-    build_pairplot(nba)
+    build_histogram(nba)
     build_scatterplot(nba)
 
 if __name__ == "__main__":
