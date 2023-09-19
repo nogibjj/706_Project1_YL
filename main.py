f0c9850d5f2e2a33a81c74e413d0e958094c4eb9
Yue Li <michaelli0914@outlook.com>
2023-09-19 04:01:33 +0000

    update report.md

--- main.py
+++ main.py
@@ -15,3 +15,21 @@ if __name__ == "__main__":
     summary = run_summary(data_=nba)
     run_scatter(nba)
     run_histogram(nba)
+
+    string = f'''
+    # This is the generated report for summary statistics and visualization for [nba-teams-2017.csv](https://github.com/nogibjj/706_Project1_YL/blob/main/nba-teams-2017.csv).
+    ## Descriptive statistics 
+
+    {summary.to_markdown()} 
+
+    ## Here are some plots to visualize relations between the important variables described in README. 
+    ### Histogram for Cumulative Points for all the teams during the season 
+     ![Alt text](figures/points_hist.png) 
+     ### Scatterplot for number of games won versus cumulative points for all teams during the season 
+     ![Alt text](figures/scatter.png)
+    '''
+
+    file_path = "./report.md"
+
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(string)
