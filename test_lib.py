e3f06c064f18e04ba5b9b1d53a0bd334640acd6b
Yue Li <michaelli0914@outlook.com>
2023-09-20 18:42:09 +0000

    change test_lib.py

--- test_lib.py
+++ test_lib.py
@@ -1,19 +1,25 @@
-# import pandas as pd
+import pandas as pd
 from lib import readfile, lib_describe, build_histogram, build_scatterplot
 
 csv_dir = "nba-teams-2017.csv"
 
-nba = readfile(file=csv_dir)
 
+def test_readfile():
+    nba = readfile(csv_dir)
+    assert isinstance(nba, pd.DataFrame)
+    assert nba.shape[0] == 30
 
 def test_describe():
-    data = nba
-    lib_describe(data)
+    nba = readfile(csv_dir)
+    assert lib_describe(nba) is not None
+    assert lib_describe(nba).shape == (8, 26)
 
 
 def test_histogram():
-    build_histogram(nba)
+    nba = readfile(csv_dir)
+    assert build_histogram(nba) is not None
 
 
 def test_scatter():
-    build_scatterplot(nba)
+    nba = readfile(csv_dir)
+    assert build_scatterplot(nba) is not None
